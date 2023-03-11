1437c2bdb9981e91d0b87744e5654dbc885eab5d
sandeshjadhav5 <jadhavs932@gmail.com>
2023-03-11 20:55:41 +0530

    changes in cors

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Testroom App");
